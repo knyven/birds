8aab83c5e62bd692565bff7e8164d2544be82067
luckylighter <vknyaz06@gmail.com>
2022-07-06 18:15:23 -0400

    2nd upload

---
+++ src/bird/AbstractBird.java
@@ -0,0 +1,48 @@
+package bird;
+
+public class AbstractBird implements BirdInterface{
+    @Override
+    public String getType() {
+        return null;
+    }
+
+    @Override
+    public String getChar() {
+        return null;
+    }
+
+    @Override
+    public boolean boolFlight() {
+        return false;
+    }
+
+    @Override
+    public boolean boolExtinct() {
+        return false;
+    }
+
+    @Override
+    public boolean boolMammal() {
+        return false;
+    }
+
+    @Override
+    public String getFood() {
+        return null;
+    }
+
+    @Override
+    public String getLocation() {
+        return null;
+    }
+
+    @Override
+    public boolean boolWater() {
+        return false;
+    }
+
+    @Override
+    public String getWaterName() {
+        return null;
+    }
+}
---
+++ src/bird/BirdInterface.java
@@ -0,0 +1,13 @@
+package bird;
+
+public interface BirdInterface {
+    String getType();
+    String getChar();
+    boolean boolFlight();
+    boolean boolExtinct();
+    boolean boolMammal();
+    String getFood();
+    String getLocation();
+    boolean boolWater();
+    String getWaterName();
+}
