08170d0b02de91c5d8303b7791ab283279d175e9
sahsahil1998 <sahsahil1997@gmail.com>
2022-07-09 15:45:08 -0400

    update

--- src/bird/ShoreBirds.java
+++ src/bird/ShoreBirds.java
@@ -4,8 +4,13 @@ import java.util.ArrayList;
 
 public class ShoreBirds extends WaterBird {
 
+    private String waterType;
+    public ShoreBirds(String type, String salientChar, boolean isExtinct, int wingNum, ArrayList<food> favFood, String waterType) {
+        super(type, salientChar, isExtinct, wingNum, favFood);
+        this.waterType = waterType;
+    }
 
-    public ShoreBirds(String type, boolean isExtinct, int wingNum, ArrayList<food> favFood, String waterType) {
-        super(type, isExtinct, wingNum, favFood, waterType);
+    public String getWaterType(){
+        return this.waterType;
     }
 }
--- src/bird/WaterBird.java
+++ src/bird/WaterBird.java
@@ -5,9 +5,10 @@ import java.util.ArrayList;
 public class WaterBird extends Bird{
     private String waterType;
 
-    public WaterBird(String type, String salientChar, boolean isExtinct, int wingNum, ArrayList<food> favFood, String waterType) {
+    public WaterBird(String type, String salientChar, boolean isExtinct, int wingNum, ArrayList<food> favFood) {
         super(type, salientChar, isExtinct, wingNum, favFood);
     }
+
 
 
 }
