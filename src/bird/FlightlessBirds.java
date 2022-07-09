9658bfe96f220bd82f872ea5b4e3d804ef8b7c4d
luckylighter <vknyaz06@gmail.com>
2022-07-09 17:46:31 -0400

    6th upload

--- src/bird/FlightlessBirds.java
+++ src/bird/FlightlessBirds.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class FlightlessBirds extends Bird{
 
-    public FlightlessBirds(String type, boolean isExtinct, int wingNum, ArrayList<food> favFood) {
-        super(type, "flightless bird", isExtinct, wingNum, String item1);
+    public FlightlessBirds(String type, boolean isExtinct, int wingNum, String item1) {
+        super(type, "flightless bird", isExtinct, wingNum, item1);
     }
 }
