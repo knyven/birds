10ee1874df47e582c91550806c0fa7c993c64f2f
sahsahil1998 <sahsahil1997@gmail.com>
2022-07-09 14:56:57 -0400

    update

--- src/bird/ConservatoryInterface.java
+++ src/bird/ConservatoryInterface.java
@@ -2,5 +2,12 @@ package bird;
 
 public interface ConservatoryInterface {
 
+    void rescueBird(Bird bird);
+    String calculateFood();
+    String printMap();
+    String printIndex();
+
+    String printSign();
+
 
 }
