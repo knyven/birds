a09b65348f8b2fca549107da7be41164fdb93142
sahsahil1998 <sahsahil1997@gmail.com>
2022-07-09 16:03:23 -0400

    update

--- src/bird/Owl.java
+++ src/bird/Owl.java
@@ -1,4 +1,11 @@
 package bird;
 
-public class Owl {
+import java.util.ArrayList;
+
+public class Owl extends Bird {
+
+
+    public Owl(String type, String salientChar, boolean isExtinct, int wingNum, ArrayList<food> favFood) {
+        super(type, "Facial disks that frame the eyes and bill", isExtinct, wingNum, favFood);
+    }
 }
