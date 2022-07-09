99f914f14c8397ab17ea4a1007627e876d0695c2
luckylighter <vknyaz06@gmail.com>
2022-07-09 14:49:38 -0400

    5th upload

---
+++ test/bird/BirdTest.java
@@ -0,0 +1,29 @@
+package bird;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BirdTest {
+
+    @Test
+    public void getType() {
+    Bird a = new
+    }
+
+    @Test
+    public void getChar() {
+    }
+
+    @Test
+    public void getWings() {
+    }
+
+    @Test
+    public void boolExtinct() {
+    }
+
+    @Test
+    public void getFood() {
+    }
+}
