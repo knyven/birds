43032587626caccc71fdcea624c215d6c89ddec7
luckylighter <vknyaz06@gmail.com>
2022-07-09 15:28:37 -0400

    6th upload

--- src/bird/WaterBird.java
+++ src/bird/WaterBird.java
@@ -5,8 +5,9 @@ import java.util.ArrayList;
 public class WaterBird extends Bird{
     private String waterType;
 
-
     public WaterBird(String type, String salientChar, boolean isExtinct, int wingNum, ArrayList<food> favFood, String waterType) {
         super(type, salientChar, isExtinct, wingNum, favFood);
     }
+
+
 }
