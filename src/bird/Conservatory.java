3875a2076a01b24076b0f5e7eaf46b82cf8a7959
luckylighter <vknyaz06@gmail.com>
2022-07-11 22:24:32 -0400

    8th upload

--- src/bird/Conservatory.java
+++ src/bird/Conservatory.java
@@ -44,45 +44,22 @@ public class Conservatory implements ConservatoryInterface{
     @Override
     public Conservatory rescueBird(AbstractBird object) {
         //
-        // if there is a compatible aviary put and the conservatory
+        // if there is a compatible aviary, and it is not full put the bird object in there
         //
         for(Aviary aviary : this.aviaryList) {
             if (aviary.isCompatible(object) && !aviary.isFull()) {
                 aviary.addBird(object);
                 return this;
             }
-
         }
-        // CASE 2:
-        // if the conservatory is completely empty we have to create a new aviary and put the bird
-        // object inside it
-        else if(this.conservatoryEmpty())
+        if(this.conservatoryFull())
         {
+            throw new IllegalStateException("Conservatory is full");
+        }
+        else {
             Aviary newAviary = (Aviary) this.makeAviary(object);
             newAviary.addBird(object);
             this.addAviary(newAviary);
-            return this;
-        }
-        // CASE 3:
-        // if conservatory is not full check for compatible aviaries to put current bird object into
-        // the aviary that fits its instance
-        else {
-            for (Aviary aviary : this.aviaryList) {
-                if (aviary.isCompatible(object) && this.conservatoryFull()) {
-                    aviary.addBird(object);
-                    return this;
-                }
-
-                // CASE 4:
-                // no compatible aviary but conservatory still has space lets make a new aviary for this bird object
-                //
-                else if (!aviary.isCompatible(object) && !this.conservatoryFull()){
-                    Aviary newAviary = (Aviary) this.makeAviary(object);
-                    newAviary.addBird(object);
-                    this.addAviary(newAviary);
-                    return this;
-                }
-            }
             return this;
         }
     }
@@ -99,7 +76,6 @@ public class Conservatory implements ConservatoryInterface{
         }
         return this;
     }
-
 
 
     public String calculateFood() {
@@ -126,71 +102,60 @@ public class Conservatory implements ConservatoryInterface{
             //access the birds in each aviary
             for(int j = 0; j < currAviary.getSize(); j++){
                 AbstractBird currBird = currAviary.birdList.get(j);
-                //if birds fav found is found in listofFood add 1 to the corresponding index in foodCounter
                 for(int f = 0; f < sizeOfFood; f++){
-                    if(currBird.getFavFood().equals(listOfFood[f])){
+                    if(currBird.getFavFood().equals((listOfFood[f]))){
                         foodCounter[f]++;
                     }
                 }
             }
         }
-        //output food counts along with birds
-        String str = "Food types \t\t\t\t Number of Birds";
-        for(int i = 0; i < sizeOfFood; i++){
-            if(foodCounter[i] > 0){
-                if(listOfFood[i].toString().length() < 10){
-                    str += listOfFood[i] + "\t\t\t\t" + foodCounter[i] + "\n";
-                }else {
-                    switch (listOfFood[i]) {
-                        case VEGETATION, OTHERBIRDS:
-                            str += listOfFood[i].toString() + "\t\t\t\t" + foodCounter[i] + "\n";
-                            break;
-                        case SMALLMAMMALS:
-                            str += listOfFood[i].toString() + "\t\t\t" + foodCounter[i] + "\n";
-                            break;
-                        case AQUATICINVERTABRATES:
-                            str += listOfFood[i].toString() + "\t" + foodCounter[i] + "\n";
-                            break;
-                    }
-                }
-            }
-        }
-        return str;
+        return null;
     }
 
-    public String printMap() {
+
+
+
+
+    @Override
+    public void printMap() {
         String map = "Conservatory contains: ";
 
-        for (int i = 0; i < this.aviaryCount; i++) {
+        for (int i = 0; i < this.aviaryList.size(); i++) {
+
+            // loop over individual aviary list containing up to 5 bird objects
             Aviary currentAviary = this.aviaryList.get(i);
-            for (int j = 0; i < this.aviaryList.get(j).getBirdNum(); j++) {
+            System.out.println();
+
+            for (int j = 0; j < currentAviary.birdList.size(); j++) {
                 AbstractBird currentBird = currentAviary.birdList.get(j);
-                map += currentBird.getName();
+                System.out.println(currentBird.getName() + " - " + currentBird.getType());
             }
-
-
         }
-        return map;
     }
 
     public String printIndex() {
         return null;
     }
 
+
+    public String printSign() {
+        return null;
+    }
+
+
     public String printSign(int indexOfAviary) {
 
         Aviary someAviary = aviaryList.get(indexOfAviary);
-        String sign = "";
-        for (int i = 0; i < someAviary.getSize(); ++i) {
+        for(int i = 0; i < someAviary.getSize(); ++i){
             AbstractBird bird = someAviary.birdList.get(i);
-            sign += "Bird " + i
+            System.out.println("Bird " + i
                     + "Name : " + bird.getName()
                     + ", Bird type: " + bird.getType()
-                    + ", Characteristics: " + bird.getSharedChar()
+                    + ", Characteristics: "
                     + ",Number of Wings:" + bird.getWingNum()
-                    + ", Favorite food to eat: " + bird.getFavFood();
+                    + ", Favorite food to eat: " + Arrays.toString(bird.getFavFood()));
         }
-        return sign;
+        return null;
     }
 
     @Override
@@ -203,7 +168,7 @@ public class Conservatory implements ConservatoryInterface{
         return this.aviaryCount == 20;
     }
 
-    @Override
+
     public boolean conservatoryEmpty() {
         return this.aviaryCount == 0;
     }
